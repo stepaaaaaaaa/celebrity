8e67bc04c4d5720d7d1ce0142a7a3a1247774dd9
stepaaaaaaaa <stepan.ohanyan45@gmail.com>
2022-10-21 14:30:50 +0600

    celebrity

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import (Flask, render_template, request)
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return render_template("shap/index.html")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
